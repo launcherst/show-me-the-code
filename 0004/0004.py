5d1f5d45c594f9d6de91911f9e87e7ff737c22e8
launcherst <pavelbuffon@126.com>
2016-08-07 09:10:09 +0800

    0004 solution

--- 0004/0004.py
+++ 0004/0004.py
@@ -1,13 +1,18 @@
 # -*-coding:utf8 -*-
 
+# 任一个英文的纯文本文件，统计其中的单词出现的个数
+
 import os,sys,string
 
-def match_word(input):
+def match_word(line):
+	words = []
 	word = ''
-	for i in range(len(input)):
-		if input[i] not in string.ascii_letters: break
-		word += input[i]
-	return word
+	for letter in line:
+		if letter in string.ascii_letters: word += letter
+		else:
+			words.append(word)
+			word = ''
+	return words
 
 if __name__ == '__main__':
 	infile_name = sys.argv[1]
@@ -22,9 +27,9 @@ if __name__ == '__main__':
 	result = {}
 	
 	for line in infile:
-		for word in line.split():
-			key = match_word(word).lower()
-			if len(key) == 0: break
+		for word in match_word(line):
+			key = word.lower()
+			if len(key) == 0: continue
 			if key in result: result[key] += 1
 			else: result[key] = 1
 	
